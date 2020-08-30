18e0947d6e84a2a979c75ba89acfa24093198444
David Velasco <davvelsan@gmail.com>
2020-08-30 14:07:16 +0200

    refactor: query execution log as a helper

--- experiments/async-pg-promise/index.js
+++ experiments/async-pg-promise/index.js
@@ -1,5 +1,6 @@
 const {
-  getPostgresConnections,
+  getConnections,
+  logQueryResults,
   queryPgPromise,
 } = require('../../utils/query-helpers');
 
@@ -11,25 +12,12 @@ const QUERY = 'SELECT * from random'
 
 /* EXECUTE ASYNC QUERIES */
 
-const startAll = new Date();
+const start = new Date();
 
-const conns = getPostgresConnections();
-const results = conns.map(
-  connection => queryPgPromise(QUERY, connection)
-);
+const conns = getConnections().postgres;
+const results = conns.map(connection => queryPgPromise(QUERY, connection));
 
 
 /* LOG QUERY TIMES */
 
-Promise
-  .all(results).then(results => {
-
-    console.log(
-      `\n/* PG-PROMISE RESULTS OVER ${conns.length} POSTGRES CONNECTIONS */\n`,
-      '\nTimes are measured in milliseconds'
-    );
-    console.log(results);
-    console.log(`FINISHED ALL QUERIES in ${new Date() - startAll}ms`);
-
-  })
-  .catch(console.err);
+logQueryResults(start, results);
--- utils/query-helpers.js
+++ utils/query-helpers.js
@@ -1,5 +1,7 @@
 /* GLOBALS */
 
+// const PgNative = require('pg-native');
+
 const pgPromise = require('pg-promise');
 const pgp = pgPromise();
 
@@ -11,9 +13,9 @@ const PORTS = range(1,5);
 /**
  * Get a map of valid postgres connection objects.
  */
-module.exports.getPostgresConnections = () => {
+module.exports.getConnections = () => {
 
-  return PORTS.map(i => ({
+  const postgres = PORTS.map(i => ({
     host: 'localhost',
     port: 5000 + i,
     user: 'admin',
@@ -21,6 +23,66 @@ module.exports.getPostgresConnections = () => {
     database: 'async_experiment',
   }));
 
+  return {
+    postgres,
+  };
+
+}
+
+
+/**
+ * Print query execution times to STDOUT.
+ * @param {Date} start start date prior to the execution of all queries
+ * @param {Promise[]} promises query results
+ */
+module.exports.logQueryResults = (start, promises) => {
+
+  Promise.all(promises).then(results => {
+
+    console.log(
+      `\n/* PG-PROMISE RESULTS OVER ${promises.length} POSTGRES CONNECTIONS */\n`,
+      '\nTimes are measured in milliseconds'
+    );
+    console.log(results);
+    console.log(`FINISHED ALL QUERIES in ${new Date() - start}ms`);
+
+  }).catch(console.err);
+}
+
+/**
+ * Asynchronously execute a specific query on the given connection.
+ * @param {string} query SQL-formatted query
+ * @param {object} connection postgres connection object
+ */
+module.exports.queryPgNative = async(query, connection) => {
+
+  const client = new PgNative();
+  const { user, password, host, port, database } = connection;
+
+  return new Promise((resolve, reject) => {
+
+    client.connect(
+      `postgresql://${user}:${password}@${host}:${port}/${database}}`,
+      (err) => {
+
+        if (err) reject(err);
+
+        const startQuery = new Date();
+        client.query(query, (err, rows) => {
+
+          if (err) reject(err);
+
+          const endQuery = new Date();
+
+          resolve({
+            port,
+            time: (endQuery - startQuery),
+          })
+
+        })
+
+      })
+  })
 }
 
 /**
