d61432fb430f5c34d778a45f1d2a55153bbec722
David Velasco <davvelsan@gmail.com>
2020-09-01 16:33:10 +0200

    fix: wrong data table filename

--- scripts/createData.sh
+++ scripts/createData.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-MAX_ROWS=${1:-"100000"}
+MAX_ROWS=${1:-"1000000"}
 DATA_DIR="../data"
 
 # Ensure we are in the "data/" working directory
 cd "$(dirname $0)"
 
 # remove any existing data
-rm -f $DATA_DIR/data.txt
+rm -f $DATA_DIR/table.txt
 
 for i in $(seq 1 $MAX_ROWS); do
 
   printf -v n "%07d" $i
-  echo -e "${n}\trandom_text_${n}" >> $DATA_DIR/data.txt
+  echo -e "${n}\trandom_text_${n}" >> $DATA_DIR/table.txt
 
 done
 
