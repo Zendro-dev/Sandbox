971eb6eedcc2ace8fad76b13b23ce8967799e089
David Velasco <davvelsan@gmail.com>
2021-01-14 17:12:06 +0100

    fix: missing migrations

---
+++ graphql-server/migrations/default-sql/20210114161147-sPARefactor.js
@@ -0,0 +1,66 @@
+'use strict';
+const dict = require('../../utils/graphql-sequelize-types');
+
+/**
+ * @module - Migrations to create and to undo a table correpondant to a sequelize model.
+ */
+module.exports = {
+
+    /**
+     * up - Creates a table with the fields specified in the the createTable function.
+     *
+     * @param  {object} queryInterface Used to modify the table in the database.
+     * @param  {object} Sequelize      Sequelize instance with data types included
+     * @return {promise}                Resolved if the table was created successfully, rejected otherwise.
+     */
+    up: function(queryInterface, Sequelize) {
+        return queryInterface.createTable('sPARefactors', {
+
+            int: {
+                type: Sequelize[dict['Int']],
+                primaryKey: true
+            },
+
+            createdAt: {
+                type: Sequelize.DATE
+            },
+
+            updatedAt: {
+                type: Sequelize.DATE
+            },
+
+            array: {
+                type: Sequelize[dict['[Boolean]']],
+                defaultValue: '[]'
+            },
+            string: {
+                type: Sequelize[dict['String']]
+            },
+            float: {
+                type: Sequelize[dict['Float']]
+            },
+            date: {
+                type: Sequelize[dict['Date']]
+            },
+            time: {
+                type: Sequelize[dict['Time']]
+            },
+            datetime: {
+                type: Sequelize[dict['DateTime']]
+            }
+
+        });
+    },
+
+    /**
+     * down - Deletes a table.
+     *
+     * @param  {object} queryInterface Used to modify the table in the database.
+     * @param  {object} Sequelize      Sequelize instance with data types included
+     * @return {promise}                Resolved if the table was deleted successfully, rejected otherwise.
+     */
+    down: function(queryInterface, Sequelize) {
+        return queryInterface.dropTable('sPARefactors');
+    }
+
+};
